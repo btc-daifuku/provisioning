09b7d34caceba40897353732dcc9f3acc1305e6f
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2017-02-08 06:47:35 +0900

    aws_flow_log set subnet_id

--- aws/1_network_storage/1_4_vpc_flow_log.tf
+++ aws/1_network_storage/1_4_vpc_flow_log.tf
@@ -1,3 +1,6 @@
+#####################################
+# Flow Log IAM
+#####################################
 resource "aws_iam_role" "flow_log" {
     name = "test_role"
     assume_role_policy = <<EOF
@@ -40,9 +43,38 @@ resource "aws_iam_role_policy" "flow_log" {
 EOF
 }
 
-resource "aws_flow_log" "flow_log" {
-  log_group_name = "${aws_cloudwatch_log_group.flow_log.name}"
+#####################################
+# Public Subnet1 Flow Log Settings
+#####################################
+resource "aws_cloudwatch_log_group" "flow_log1" {
+  name = "awslogs-${var.app_name}-vpc-flow-log-public1"
+}
+
+resource "aws_flow_log" "flow_log1" {
+  log_group_name = "${aws_cloudwatch_log_group.flow_log1.name}"
   iam_role_arn = "${aws_iam_role.flow_log.arn}"
-  vpc_id = "${aws_vpc.vpc.id}"
   traffic_type = "ALL"
+  subnet_id = "${aws_subnet.public-subnet1.id}"
+
+  depends_on = [
+    "aws_subnet.public-subnet1"
+  ]
+}
+
+#####################################
+# Public Subnet2 Flow Log Settings
+#####################################
+resource "aws_cloudwatch_log_group" "flow_log2" {
+  name = "awslogs-${var.app_name}-vpc-flow-log-public2"
+}
+
+resource "aws_flow_log" "flow_log2" {
+  log_group_name = "${aws_cloudwatch_log_group.flow_log2.name}"
+  iam_role_arn = "${aws_iam_role.flow_log.arn}"
+  traffic_type = "ALL"
+  subnet_id = "${aws_subnet.public-subnet2.id}"
+
+  depends_on = [
+    "aws_subnet.public-subnet2"
+  ]
 }
